cac04423251f4bc0bb522e337774b163e6bc47de
waidandeprachi <waidandeprachi@gmail.com>
2024-07-30 11:09:55 +0530

    file 6

--- Automation/src/test/java/tests/EditProfileDetails.java
+++ Automation/src/test/java/tests/EditProfileDetails.java
@@ -75,7 +75,7 @@ public class EditProfileDetails extends BaseTest {
 
         //Search user and edit its job role
         for (UserDetails user : userDetailsList) {
-            if (userDetailsList.indexOf(user) >= 0) {
+            if (userDetailsList.indexOf(user) >= 424) {
                 adminPage.searchUser(user.getFirst_name());
                 if (adminPage.isUserPresentInSearchList(user.getFull_name())) {
                     adminPage.clickOnEditUserProfileButton(user.getFull_name());
